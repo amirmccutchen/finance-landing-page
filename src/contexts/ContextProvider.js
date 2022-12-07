8f42906fbff4bfb77a89a7fbb6b4f1267e081604
Amir McCutchen <3amproduced@gmail.com>
2022-12-06 22:34:45 -0500

    started navigation bar and added context api

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -14,11 +14,22 @@ const initialState = {
 export const ContextProvider = ( { children } ) => {
     const [activeMenu, setActiveMenu] = useState(true);
 
+    // handleClick is for navbar buttons, handles the clicked/unclicked state of the button and adjusts content accordingly
+
+    const [isClicked, setIsClicked] = useState(initialState)
+
+    function handleClick(clicked) {
+        setIsClicked({ ...initialState, [clicked]: true });
+    }
+
     return (
         <StateContext.Provider
         value = {{ 
             activeMenu,
             setActiveMenu,
+            isClicked,
+            setIsClicked,
+            handleClick,
         }}>
             {children}
         </StateContext.Provider>
