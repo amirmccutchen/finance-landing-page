9f5df62d9dfd7b84cc669d12e677038bcc964351
Amir McCutchen <3amproduced@gmail.com>
2023-01-14 13:11:29 -0500

    created components

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from 'react'
+import styles from './style';
+import { Billing , Business, Button, CardDeals, Clients, CTA, Feedback, Footer, GetStarted, Hero, Navigation, Stats, Testimonials } from './components';
 
 const App = () => {
   return (
-    <div>App</div>
-  );
-};
+    <div className = 'bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navigation />
+        </div>
 
-export default App;
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeals />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
